7d845c8fe0019775b9f74e377cfd0e1ee33dc150
uzytkownik4325 <dlr92qe14ydxnlcvdehz5omi9ga2s0@bots.bitbucket.org>
2023-12-16 20:29:55 +0100

    Aktualizacja

--- programy/pytekst/uruchom_pytekst.py
+++ programy/pytekst/uruchom_pytekst.py
@@ -1,7 +1,7 @@
 from pytekst import *
 
-wejscie = "/home/qwerty891/Pulpit/strona_internetowa/tekst/bankowy-kredyt-gotowkowy.txt"
-wyjscie = "/home/qwerty891/Pulpit/strona_internetowa/tekst/bankowy-kredyt-gotowkowy.html"
+wejscie = "/home/qwerty891/Pulpit/strona_internetowa/tekst/kredyt-w-mbanku.txt"
+wyjscie = "/home/qwerty891/Pulpit/strona_internetowa/tekst/kredyt-w-mbanku.html"
 
 tekst = Tekst(wejscie, wyjscie)
 tekst.ustaw_poczatek_akapitu("<p>")
