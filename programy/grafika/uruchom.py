f0d9000d10f2d5955275db5550f18e60b051f7e3
uzytkownik4325 <dlr92qe14ydxnlcvdehz5omi9ga2s0@bots.bitbucket.org>
2024-03-11 02:21:33 +0100

    Aktualizacja.

---
+++ programy/grafika/uruchom.py
@@ -0,0 +1,17 @@
+import sys
+from PIL import Image, ImageDraw
+
+tlo = "rgb(255,255,254)"
+rozdzielczosc = (300, 300)
+
+proksy = (100, 100)
+punkt = "rgb(0,0,0)"
+
+image = Image.new("RGB", rozdzielczosc, tlo)
+
+rysunek = ImageDraw.Draw(image)
+rysunek.point(proksy, punkt)
+
+
+image.save("punkt.png")
+image.show()
