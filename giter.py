409bcf6427990d86fbbda35434875764dbe7f1fa
uzytkownik4325 <dlr92qe14ydxnlcvdehz5omi9ga2s0@bots.bitbucket.org>
2024-03-14 22:47:11 +0100

    Giter_-_aktualizacja

--- giter.py
+++ giter.py
@@ -2,6 +2,22 @@ import os
 
 class Giter:
 
+    def instaluj_na_linuksie(self):
+        self.wykonaj("sudo apt-get update")
+        self.wykonaj("sudo apt-get install git")
+
+    def wersja(self):
+        self.wykonaj("git --version")
+
+    def utworz_klucz_na_linuksie(self, email):
+        self.wykonaj("ssh-keygen -t rsa -b 4096 -C " + email)
+
+    def sprawdz_wywolanie(self):
+        self.wykonaj("eval \"(ssh-agent -s)\"")
+
+    def archiwum(self):
+        self.wykonaj("git archive --format=tar HEAD")
+
     def inicjuj(self):
         self.wykonaj("git init")
 
@@ -19,6 +35,12 @@ class Giter:
 
     def lista_galezi(self):
         self.wykonaj("git branch")
+
+    def lista_zdalnych_galezi(self):
+        self.wykonaj("git branch -r")
+
+    def lista_wszystkich_galezi(self):
+        self.wykonaj("git branch -a")
 
     def stworz_galaz(self, nazwa):
         self.wykonaj("git checkot -b " + nazwa)
@@ -40,6 +62,69 @@ class Giter:
 
     def wczytaj(self):
         self.wykonaj("git push")
+
+    def status(self):
+        self.wykonaj("git status")
+
+    def zmiany(self):
+        self.wykonaj("git log")
+
+    def cofnij_zmiany(self):
+        self.wykonaj("git checkout .")
+
+    def kopiuj(self, repozytorium):
+        self.wykonaj("git clone " + repzytorium)
+
+    def kopiuj_do_folderu(self, repozytorium, folder):
+        self.wykonaj("git clone " + repozytorium + " " + folder)
+
+    def kopiuj_tag(self, repozytorium, tag):
+        self.wykonaj("git clone --branch " + tag + " " + repozytorium)
+
+    def kopiuj_od_wczytania(self, repozytorium, numer_wiadomosci):
+        self.wykonaj("git clone -depth=" + numer_wiadomosci + " " + repozytorium)
+
+    def ustaw_email(self, adres_email):
+        self.wykonaj("git config --global user.email " + adres_email)
+
+    def ustaw_uzytkownika(self, uzytkownik):
+        self.wykonaj("git config --global user.name " + uzytkownik)
+
+    def ustaw_edytor(self, edytor):
+        self.wykonaj("git config --global code.editor " + edytor)
+
+    def ustaw_tag(self, tag):
+        self.wykonaj("git tag " + tag)
+
+    def zmiany_pliku(self, nazwa):
+        self.wykonaj("git blame " + nazwa)
+
+    def wyczysc(self):
+        self.wykonaj("git clean -n")
+
+    def wyczysc_pamiec(self):
+        self.wykonaj("git clean -f")
+
+    def na_koniec(self):
+        self.wykonaj("git revert HEAD")
+
+    def cofnij_zmiany(self):
+        self.wykonaj("git reset")
+
+    def resetuj(self):
+        self.wykonaj("git reset --hard")
+
+    def resetuj_i_zapisz(self):
+        self.wykonaj("git reset --soft")
+
+    def resetuj_wczytanie(self, wczytanie):
+        self.wykonaj("git reset --soft " + wczytanie)
+
+    def usun_wczytanie(self, numer_wczytania_od_gory):
+        self.wykonaj("git reset --hard HEAD~" + numer_wczytania_od_gory)
+
+    def usun_plik(self, plik):
+        self.wykonaj("git rm " + plik)
 
     def wykonaj(cls, polecenie):
         os.system(polecenie)
