a2ded8a2f18d087e2f2a2ef3e8ff2a35a0507479
uzytkownik4325 <dlr92qe14ydxnlcvdehz5omi9ga2s0@bots.bitbucket.org>
2024-03-26 22:06:41 +0100

    @@##_Giter_-_aktualizacja_##@@

---
+++ programy/plikowo/algorytmy/fragmenty/fragmenty.py
@@ -0,0 +1,21 @@
+class Fragmenty:
+
+    def potnij(self, fraza, separator):
+        try:
+            frazy = fraza.split(separator)
+            return frazy
+        except:
+            print("Klasa Fragmenty, metoda potnij(). \n" +
+            "Nie można pociąć fragmentów.")
+            return False
+
+    def sortuj(self, frazy):
+        try:
+            # do dopracowania
+            indeksy = []
+            kolejnosc = []
+            return frazy
+        except:
+            print("Klasa Fragmenty, metoda sortuj(). \n" +
+            "Nie można posortować fragmentów.")
+            return False
---
+++ programy/plikowo/algorytmy/fragmenty/uruchom.py
@@ -0,0 +1,8 @@
+from fragmenty import *
+
+tekst = "Szła dzieweczka do laseczka, do zielonego, ho, ho."
+
+fragmenty = Fragmenty()
+
+frazy = fragmenty.potnij(tekst, " ")
+print(frazy)
